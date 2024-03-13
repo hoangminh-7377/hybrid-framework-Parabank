5f277d4bd5b9ca02f4ff9a1e3d6145babaf01073
hoangminh.nguyen7377 <hoangminh.nguyen7377@gmail.com>
2024-03-13 20:29:06 +0700

    commit

--- src/main/java/pageUIs/HomePageUI.java
+++ src/main/java/pageUIs/HomePageUI.java
@@ -3,5 +3,5 @@ package pageUIs;
 public class HomePageUI {
     public static final String WELCOME_TEXT = "xpath=//p[@class='smallText']/b[contains(text(),'Welcome')]";
     public static final String ACCOUNT_SERVICES_ITEM_DYNAMIC = "xpath=//h2[text()='Account Services']/following-sibling::ul//a[text()='%s']";
-    public static final String PAGE_TITLE_DYNAMIC = "xpath=//h1[@class='title'][text()='%s']";
+    public static final String PAGE_TITLE_DYNAMIC = "xpath=//h1[@class='title'][contains(text(),'%s')]";
 }
